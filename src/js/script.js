290a0b593b3f5cb378d17abe25ea95f3eb98fc49
Tatsiana <tati_website@mail.ru>
2021-09-12 16:15:30 +0300

    Ready continue links

--- src/js/script.js
+++ src/js/script.js
@@ -4,24 +4,93 @@ window.addEventListener('DOMContentLoaded', () => {
     const slidesWrapper = document.querySelector('.reviews__slider-wrapper'),
           slides = slidesWrapper.querySelectorAll('.reviews__slide'),
           slidesField = slidesWrapper.querySelector('.reviews__slider-inner'),
-          slideWidth = window.getComputedStyle(slidesWrapper).width;
+          slideWidth = window.getComputedStyle(slidesWrapper).width,
+          plansInner = document.querySelector('.plans__inner'),
+          plans = plansInner.querySelectorAll('.plan'),
+          plansButton = document.querySelector('.plans__btn');
 
-    let slideIndex = 1;
-    let offset = 0;
+    designPlan();
+    createSlider();
 
-    slidesField.style.width = 100 * slides.length + '%';
-    slides.forEach(slide => {
-        slide.style.width = slideWidth;
-    });
 
-    slidesWrapper.addEventListener('click', () => {
+    
 
-        if(offset == +slideWidth.slice(0, slideWidth.length - 2) * (slides.length - 1)){
-            offset = 0;
-        } else {
-            offset += +slideWidth.slice(0, slideWidth.length - 2);
+    //PlansChecking
+    plansInner.addEventListener('click', (e) => {
+        if(e.target){
+            plans.forEach((plan) => {
+                plan.querySelector('.plan__info').classList.remove('active');
+            });
+            let targetPlan = e.target.closest('.plan__info');
+            targetPlan.classList.add('active');
         }
-
-        slidesField.style.transform = `translateX(-${offset}px)`;
     });
+
+    //Go To Continue
+    plansButton.addEventListener('click', () => {
+        plansButton.setAttribute('href', '');
+        plans.forEach((plan) => {
+            if(plan.querySelector('.plan__info.active')){
+               let activePlan = plan.querySelector('.plan__info.active');
+               let link = activePlan.getAttribute('data-link'); 
+               plansButton.setAttribute('href', link);
+            }
+        });
+    });
+
+    //AdaptiveLines
+    // let display = document.documentElement.clientWidth;
+    // document.querySelectorAll('.accent-offer__line>svg').forEach((line) => {
+    //     line.style.width = display;
+    // });
+
+    //Slider
+    function createSlider(){
+        let slideIndex = 1;
+        let offset = 0;
+        let display = document.documentElement.clientWidth;
+        let slideMargin = 0;
+
+        slides.forEach(slide => {
+            slide.style.width = display / 100 * 90;
+        });
+
+        slidesField.style.width = 100 * slides.length + '%';
+        slidesField.style.transform = 'translateX(-22%)';
+
+        slides.forEach(slide => {
+            slide.style.width = display / 100 * 90;
+        });
+
+        slidesField.style.width = 100 * slides.length + '%';
+        slidesField.style.transform = 'translateX(-22%)';
+
+        slidesWrapper.addEventListener('click', () => {
+    
+            if(offset == +slideWidth.slice(0, slideWidth.length - 2) * (slides.length - 1)){
+                offset = 0;
+            } else {
+                offset += +slideWidth.slice(0, slideWidth.length - 2);
+            }
+    
+            slidesField.style.transform = `translateX(-${offset}px)`;
+        });
+    }
+    
+    //PlansDesign
+    function designPlan(){
+        plans.forEach((plan) => {
+            if(!plan.querySelector('.plan__header')){
+                plan.querySelector('.plan__info').classList.add('radius'); 
+            } else if(plan.querySelector('.plan__footer') && plan.querySelector('.plan__header')){
+                plan.querySelector('.plan__header').classList.add('bright'); 
+            }
+        });
+    }
+
+
+   
+
+    
+
 });
